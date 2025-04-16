2ad61862dd6a5b4f712a0b5a0d669b4d84bd9cd8
Alisa Valiullina <avaliull@f0r2s2.codam.nl>
2025-04-16 13:54:43 +0200

    making some useful utils

--- src/fdf_utils.c
+++ src/fdf_utils.c
@@ -26,3 +26,65 @@ void	free_2d_arr(void **arr)
 	}
 }
 
+int	ft_isspace(int c)
+{
+	unsigned char	u_c;
+
+	u_c = (unsigned char) c;
+	if ((u_c >= '\t' && u_c <= '\r') || u_c == ' ')
+		return (true);
+	return (false);
+}
+
+int	ft_isupper(int c)
+{
+	unsigned char	u_c;
+
+	u_c = (unsigned char) c;
+	if (u_c >= 'A' && u_c <= 'Z')
+		return (true);
+	return (false);
+}
+
+int	ft_islower(int c)
+{
+	unsigned char	u_c;
+
+	u_c = (unsigned char) c;
+	if (u_c >= 'A' && u_c <= 'Z')
+		return (true);
+	return (false);
+}
+
+long	ft_base_convert_l(char *s, size_t str_len, int base)
+{
+	size_t	i;
+	long	converted_num;
+
+	i = 0;
+	while (i < str_len)
+		
+}
+
+long	ft_strtol(void *nptr, char **endptr, int base)
+{
+	ssize_t			i;
+	ssize_t			num_start;
+	int				sign;
+	unsigned char	*str;
+	
+	str = (unsigned char *) nptr;
+	sign = -1;
+	i = 0;
+	while (ft_isspace(str[i]))
+		i++;
+	if (str[i] == '-')
+		sign = -sign;
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+	num_start = i;
+	while (ft_isdigit(str[i]) || ft_isalpha(str[i]))
+		i++;
+	if (endptr)
+		*endptr = &nptr[i];
+}
